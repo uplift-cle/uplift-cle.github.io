fd248162b5b7f8d992d9b06d6ce9ca0c83c7a8dc
cqdetdisc <136763361+pxs841@users.noreply.github.com>
2025-06-17 17:08:33 -0400

    chore: correct metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'uplift.cle',
+  description: 'Addressing acute, systemic issues within Cleveland through innovative, interdisciplinary, and research-driven solutions.',
 }
 
 export default function RootLayout({
