d301ad730633ddc56cb6c8d85b29083ca78f00e2
cqdetdisc <136763361+pxs841@users.noreply.github.com>
2025-06-18 20:35:25 -0400

    feat: basic SEO metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,69 @@ import './globals.css'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
-    title: 'uplift.cle',
-    description: 'Addressing acute, systemic issues within Cleveland through innovative, interdisciplinary, and research-driven solutions.',
-}
+  title: 'uplift.cle | Transforming Cleveland',
+  description:
+    'uplift.cle addresses systemic issues in Cleveland through interdisciplinary collaboration, data-driven research, and sustainable, community-first solutions.',
+  keywords: [
+    'Cleveland',
+    'urban innovation',
+    'community research',
+    'systemic issues',
+    'interdisciplinary solutions',
+    'social impact',
+    'Uplift CLE',
+    'public policy',
+    'research-driven solutions',
+    'Case Western Reserve University',
+    'Students',
+    ''
+  ],
+  authors: [{ name: 'Uplift.CLE Team', url: 'https://uplift.cle' }],
+  creator: 'Uplift.CLE',
+  publisher: 'Uplift.CLE',
+
+  openGraph: {
+    title: 'Uplift.CLE | Transforming Cleveland',
+    description:
+      'Driving change in Cleveland through innovative, research-based, and community-focused approaches.',
+    url: 'https://uplift-cle.vercel.app',
+    siteName: 'uplift.cle',
+    locale: 'en_US',
+    type: 'website',
+    images: [
+      {
+        url: 'https://avatars.githubusercontent.com/u/216775761?s=200&v=4',
+        width: 1200,
+        height: 630,
+        alt: 'Uplift.CLE — Transforming Cleveland',
+      },
+    ],
+  },
+
+//   twitter: {
+//     card: 'summary_large_image',
+//     title: 'Uplift.CLE | Transforming Cleveland Through Innovation & Research',
+//     description:
+//       'Tackling systemic issues in Cleveland with data, innovation, and community partnerships.',
+//     site: '@upliftcle',
+//     creator: '@upliftcle',
+//     images: ['https://avatars.githubusercontent.com/u/216775761?s=200&v=4'],
+//   },
+
+  metadataBase: new URL('https://uplift-cle.vercel.app'),
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-snippet": -1,
+      "max-image-preview": 'large',
+      "max-video-preview": -1,
+    },
+  },
+};
+
 
 export default function RootLayout({
     children,
